3a3f48c590094cee1e71aa6ed910c5a46cc82fb3
github-action-benchmark <github@users.noreply.github.com>
2024-09-05 22:30:14 +0000

    add Benchmark (pytest) benchmark result for b20708b02d4920088d77acba2425b6bbe127da91

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1725575414881,
+  "repoUrl": "https://github.com/jvllmr/fastapi-deferred-init",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jan@vllmr.dev",
+            "name": "Jan Vollmer",
+            "username": "jvllmr"
+          },
+          "committer": {
+            "email": "jan@vllmr.dev",
+            "name": "Jan Vollmer",
+            "username": "jvllmr"
+          },
+          "distinct": true,
+          "id": "b20708b02d4920088d77acba2425b6bbe127da91",
+          "message": "use cache in publish action",
+          "timestamp": "2024-09-06T00:25:51+02:00",
+          "tree_id": "1c97f4970f086f7aaa524eab6455912c8f2ee796",
+          "url": "https://github.com/jvllmr/fastapi-deferred-init/commit/b20708b02d4920088d77acba2425b6bbe127da91"
+        },
+        "date": 1725575414101,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_lib.py::test_basic[True]",
+            "value": 3.5928785491043467,
+            "unit": "iter/sec",
+            "range": "stddev: 0.01066050745232246",
+            "extra": "mean: 278.32836160000056 msec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
