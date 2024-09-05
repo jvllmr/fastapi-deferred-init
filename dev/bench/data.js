0b3289e766cd7268d167f7b173361f8072110249
github-action-benchmark <github@users.noreply.github.com>
2024-09-05 22:36:48 +0000

    add Benchmark (pytest) benchmark result for 84ac0ffc6914decebcaed03cc07a03a036b42616

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725575632232,
+  "lastUpdate": 1725575808904,
   "repoUrl": "https://github.com/jvllmr/fastapi-deferred-init",
   "entries": {
     "Benchmark": [
@@ -69,6 +69,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.165228249789474",
             "extra": "mean: 2.7860401310000014 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "84ac0ffc6914decebcaed03cc07a03a036b42616",
+          "message": "Update all non-major dependencies (#10)\n\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>",
+          "timestamp": "2024-09-06T00:35:53+02:00",
+          "tree_id": "85db863cbaf1cfa1f4040c23b0a5474dc04e77a3",
+          "url": "https://github.com/jvllmr/fastapi-deferred-init/commit/84ac0ffc6914decebcaed03cc07a03a036b42616"
+        },
+        "date": 1725575808660,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_lib.py::test_basic[True]",
+            "value": 3.6474187530432998,
+            "unit": "iter/sec",
+            "range": "stddev: 0.008811523185243201",
+            "extra": "mean: 274.16649080000184 msec\nrounds: 5"
+          },
+          {
+            "name": "tests/test_lib.py::test_basic[False]",
+            "value": 0.34568044822202865,
+            "unit": "iter/sec",
+            "range": "stddev: 0.1946502287670591",
+            "extra": "mean: 2.8928451266000024 sec\nrounds: 5"
           }
         ]
       }
